82d72f1f31b3cc48122cb56f2edb1a8e18f11b9d
Leland Tran <lelandtran@ymail.com>
2016-05-30 17:25:14 -0700

    first commit with tutorial foundation

---
+++ src/main/java/stocks/StocksController.java
@@ -0,0 +1,21 @@
+package stocks;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class StocksController {
+
+	@RequestMapping("/helloworld")
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model){
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+}
